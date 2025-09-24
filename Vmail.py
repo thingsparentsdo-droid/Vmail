3b6abdf1029833c57cd336bbb9f233114c1f70d7
VivAlgorithm1 <thingsparentsdo@gmail.com>
2025-09-23 17:20:00 -0700

    Update Vmail.py

--- Vmail.py
+++ Vmail.py
@@ -43,7 +43,7 @@ while True:
              if curent_user_data["passtemp"] == vmailpass:
                 for i in curent_user_data["user_Vmail"]:
                     prewiew = "\033[1m" + i["subject"] + "\033[0m | " + i["content"][0:70]
-                    print(f"\n{mesageindex} | {i["sender"]} | {prewiew}\n")
+                    print(f"\n{mesageindex} | {i[sender]} | {prewiew}\n")
                     mesageindex += 1
              mesageindex = 1
              
